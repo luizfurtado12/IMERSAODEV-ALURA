13a7314ce918ea4d0ebe5691edb2cc671ddc9302
Luiz Felipe <luizfelipelimafurtado@gmail.com>
2022-09-24 11:05:56 -0300

    select

--- Aula_02/script.js
+++ Aula_02/script.js
@@ -1,3 +1,4 @@
+
 function nina(even) {
     if (even.code == 'Enter') {
         document.getElementById("search").click();
@@ -5,6 +6,13 @@ function nina(even) {
 };
 
 function Converter() {
+    var out = document.getElementById("moeda-1");
+    out.addEventListener('change', function(){
+        console.log(out.value)
+    })
+    // var put = document.getElementById("moeda-2");
+    // console.log(out)
+    // console.log(put)
     var valor = parseFloat(document.getElementById("valor").value)
     var valorConvertido = valor * 5.25
     var msg = "O preço ficou R$ " + valorConvertido.toFixed(2).replace('.', ',')
